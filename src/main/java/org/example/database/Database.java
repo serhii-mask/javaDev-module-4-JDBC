29667e676e5dfc59aa59bd6d5351dbcdd94b0748
Serhii <serhii.mask@gmail.com>
2023-10-10 09:58:11 +0300

    feat: add Database and Props

---
+++ src/main/java/org/example/database/Database.java
@@ -0,0 +1,57 @@
+package org.example.database;
+
+import org.example.props.Props;
+
+import java.sql.*;
+
+public class Database {
+    private static final Database INSTANCE = new Database();
+
+    private Connection connection;
+
+    private Database() {
+        String connectionUrl = Props.getConnectionUrl();
+
+        try {
+            connection = DriverManager.getConnection(connectionUrl);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Database getInstance() {
+        return INSTANCE;
+    }
+
+    public int executeUpdate(String sql) {
+        try (Statement st = connection.createStatement()) {
+            return st.executeUpdate(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+
+            return -1;
+        }
+    }
+
+    public int initDB(String sql) {
+        try (Statement st = connection.createStatement()) {
+            return st.executeUpdate(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+
+            return -1;
+        }
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public void close() {
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
